bba4d4a8d5cfe62a7d6f7daa8434e3d2ebee4f3b
Adrian0506 <60869100+Adrian0506@users.noreply.github.com>
2022-09-17 19:27:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client()
+
+
+client.on("Ready", () => {
+    console.log('Bot is active.')
+})
+
+
+
+
+
+
+client.login('bot key')
